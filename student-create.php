a533936a8c2a9d925f422b656fbe105a59aac0ad
RaZzLuK <165956742+RaZzLuK@users.noreply.github.com>
2024-04-08 17:04:08 -0500

    Update student-create.php

--- student-create.php
+++ student-create.php
@@ -7,8 +7,14 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <title>AGREGAR ESTUDIANTES</title>
+    <link rel="icon" href="logos.png" type="image/png">
+    <style>
+        .back-btn {
+            margin-right: 10px; /* Ajusta este valor según necesites */
+        }
+    </style>
 </head>
 <body>
 
@@ -21,7 +27,16 @@ session_start();
             <div class="card">
                 <div class="card-header">
                     <h4>AGREGAR ESTUDIANTE
-                        <a href="index.php" class="btn btn-danger float-end">ATRÁS</a>
+                    <div class="btn-group float-end" role="group">
+                                <button id="fullscreen-toggle" class="btn btn-primary">
+                                    <i class="bi bi-arrows-fullscreen"></i>
+                                </button>
+                                <button id="theme-toggle" class="btn btn-primary">
+                                    <i id="theme-icon" class="bi bi-moon-fill"></i>
+                                </button>
+                            </div>
+
+                        <a href="index.php" class="btn btn-danger float-end back-btn">ATRÁS</a>
                     </h4>
                 </div>
                 <div class="card-body">
@@ -63,6 +78,58 @@ session_start();
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script>
+    // Función para cambiar entre temas claro y oscuro
+    function toggleTheme() {
+        var body = document.body;
+        if (body.classList.contains('bg-light')) {
+            body.classList.remove('bg-light');
+            body.classList.add('bg-dark');
+            document.getElementById('theme-icon').classList.remove('bi-moon-fill');
+            document.getElementById('theme-icon').classList.add('bi-sun-fill');
+            // Guardar preferencia de tema en la sesión
+            sessionStorage.setItem('theme', 'dark');
+        } else {
+            body.classList.remove('bg-dark');
+            body.classList.add('bg-light');
+            document.getElementById('theme-icon').classList.remove('bi-sun-fill');
+            document.getElementById('theme-icon').classList.add('bi-moon-fill');
+            // Guardar preferencia de tema en la sesión
+            sessionStorage.setItem('theme', 'light');
+        }
+    }
 
+    // Función para activar la pantalla completa
+    function toggleFullscreen() {
+        var elem = document.documentElement;
+        if (!document.fullscreenElement) {
+            elem.requestFullscreen().catch(err => {
+                console.log(err.message);
+            });
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            }
+        }
+    }
+
+    // Escucha el evento de clic en el botón de cambio de tema
+    document.getElementById('theme-toggle').addEventListener('click', function() {
+        toggleTheme();
+    });
+
+    // Escucha el evento de clic en el botón de pantalla completa
+    document.getElementById('fullscreen-toggle').addEventListener('click', function() {
+        toggleFullscreen();
+    });
+
+    // Verificar si hay preferencia de tema guardada en la sesión y aplicarla al cargar la página
+    window.addEventListener('load', function() {
+        var theme = sessionStorage.getItem('theme');
+        if (theme === 'dark') {
+            toggleTheme();
+        }
+    });
+</script>
 </body>
 </html>
